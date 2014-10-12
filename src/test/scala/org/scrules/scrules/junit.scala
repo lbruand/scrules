ffb545525c047e4291c7b65cc7617465a885bb7f
Lucas Bruand <l.bruand.pro@gmail.com>
2014-10-12 11:13:43 +0200

    update

--- src/test/scala/org/scrules/scrules/junit.scala
+++ src/test/scala/org/scrules/scrules/junit.scala
@@ -11,62 +11,56 @@ import scala.reflect.runtime.{universe=>ru}
 // What is a RuleSet : It is a piece-wise definition of a Function from one Typ
 // We can further restrict the input and output type to enumerations and compos
 
-class Rule(val name: String)
+class Rule
   extends annotation.StaticAnnotation
   
 abstract class Matcher[Input] extends (Input => Boolean)
 
-case class RuleFunction/*[Input, Output]*/(
+case class RuleCase/*[Input, Output]*/(
     var label: String = "")
     /* /*,
     val matcher : (Input => Boolean),
     val result : Output,
     val salience : Int = 0*/*/
-
-class RuleSetBase {
-  @Rule("rule0")
-  def rule0 : RuleFunction = new RuleFunction()
-  @Rule("rule1")
-  def rule1 : RuleFunction = new RuleFunction()
-}
-class RuleSet extends RuleSetBase {
-	@Rule("rule1")
-    override def rule1 : RuleFunction = new RuleFunction()
-	
-	@Rule("rule2")
-    def rule2 : RuleFunction = new RuleFunction()
-}
-
-object Rule {
-  
-  def listProperties[T: TypeTag]: List[(TermSymbol, Annotation)] = {
-	  // a field is a Term that is a Var or a Val
-	  val fields = typeOf[T].members.collect{ case s: TermSymbol => s }.
-	    filter(s => s.isMethod)
-	
-	  // then only keep the ones with a MyProperty annotation
-	  fields.flatMap(f => f.annotations.find(_.tpe =:= typeOf[Rule]).
-	    map((f, _))).toList
-	}
-  
-  def getTypeTag[T: ru.TypeTag](obj: T) = ru.typeTag[T]
-  def extractRules(ruleSet : RuleSet) : List[RuleFunction] = {
-    val m = ru.runtimeMirror(getClass.getClassLoader)
-    val im = m.reflect(ruleSet)
-    val tpe = getTypeTag(ruleSet).tpe
-    tpe.members.flatMap(symbol => {
-      if (symbol.isMethod && symbol.asMethod.returnType.equals(typeOf[RuleFunction])) {
-	    val methodMirror = im.reflectMethod(symbol.asMethod)
-	    val name : String= symbol.asMethod.fullName
-	    val rule : RuleFunction = methodMirror.apply().asInstanceOf[RuleFunction]
-	    rule.label = name
-	    Some(rule)
-      } else {
-    	None
-      }
-    }).toList
     
+class RuleSet {
+
+}
+class RuleSetBase extends RuleSet {
+  @Rule
+  def rule0 : RuleCase = new RuleCase()
+  @Rule
+  def rule1 : RuleCase = new RuleCase()
+}
+class RuleSetExtended extends RuleSetBase {
+	@Rule
+    override def rule1 : RuleCase = new RuleCase()
+	
+	@Rule
+    def rule2 : RuleCase = new RuleCase()
+}
+
+
+object RuleCompiler {
+  def getTypeTag[T: ru.TypeTag](obj: T) = ru.typeTag[T]
+  def extractRules(ruleSet : RuleSetExtended) : List[RuleCase] = {
+	val m = ru.runtimeMirror(getClass.getClassLoader)
+	val im = m.reflect(ruleSet)
+	val tpe = getTypeTag(ruleSet).tpe
+	tpe.members.filter(_.isMethod).filter(_.annotations.exists(_.tpe =:= typeOf[Rule])).flatMap(symbol => {
+	    val method = symbol.asMethod 
+		if (method.returnType.equals(typeOf[RuleCase])) {
+			val methodMirror = im.reflectMethod(method)
+			val name : String= symbol.asMethod.fullName
+			val rule : RuleCase = methodMirror.apply().asInstanceOf[RuleCase]
+			rule.label = name
+			Some(rule)
+		} else {
+			None
+		}
+	}).toList
   }
+
 }
 
 
@@ -75,8 +69,7 @@ class MyTest {
 
     @Test
     def testOK() = {
-      System.out.println(Rule.extractRules(new RuleSet()))
-      assertTrue(true)
+      assertEquals(3, RuleCompiler.extractRules(new RuleSetExtended()).size)
     }
 
 }
