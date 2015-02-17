7a1a6ad62978004861cb8b7fc2e9e70dec2abcd9
Lucas Bruand <l.bruand.pro@gmail.com>
2015-02-17 22:04:51 +0100

    moved stuff

--- src/test/scala/org/scrules/scrules/set/usingset.scala
+++ src/test/scala/org/scrules/scrules/set/usingset.scala
@@ -3,6 +3,9 @@ import org.junit._
 import org.junit.Assert._
 import scala.collection.immutable.TreeSet
 import org.slf4j.LoggerFactory
+import org.scrules.scrules.RuleSet
+import org.scrules.scrules.RuleCase
+
 
 /*
  * TODO :
@@ -34,78 +37,6 @@ case class EqExpr[Input, Value](val projection : (Input => Value), val expectedV
   def apply(input : Input) : Boolean = expectedValue.equals(projection.apply(input))
   
   override def compose[A](g : A => Input) : MatchExpr[A] = new EqExpr(projection.compose(g), expectedValue)
-}
-
-case class RuleCase[Input, Output]
-			(var label : String,
-			 val matchExpr : MatchExpr[Input],
-			 override val returnValue : Output,
-			 val salience : Int = 0)
-			 extends ConstantPartialFunction[Input, Output](returnValue) with Ordered[RuleCase[Input, Output]] {
-  def isDefinedAt(x : Input) = matchExpr.apply(x)
-  
-  def compare(that: RuleCase[Input, Output]): Int = this.salience compare that.salience // TODO Maybe use the label to totalise the order.
-  
-  override def andThen[OtherOutput](other : Function1[Output, OtherOutput]) : RuleCase[Input, OtherOutput] = 
-    new RuleCase[Input, OtherOutput](this.label, this.matchExpr, other.apply(this.returnValue), this.salience)
-
-  override def compose[PreInput](g : PreInput => Input) : RuleCase[PreInput, Output] = new RuleCase[PreInput, Output](this.label,
-		  																this.matchExpr.compose(g), this.returnValue, this.salience)		  																
-}
-
-
-case class RuleSet[Input, Output](val rules : Map[String, RuleCase[Input, Output]], val defaultResult : Output) 
-						extends Function[Input, Output] {
-  def logger = LoggerFactory.getLogger(classOf[RuleSet[Input, Output]])
-  
-  lazy val runtime = new TreeSet[RuleCase[Input, Output]]()(new Ordering[RuleCase[Input, Output]] {
-    def compare(ac1: RuleCase[Input, Output], ac2: RuleCase[Input, Output]): Int = {
-      ac2.salience compare ac1.salience
-    }
-  }) ++ rules.values
-  
-  def findRule(input : Input) : Option[RuleCase[Input, Output]] = runtime.find(_.isDefinedAt(input))
-  
-  def apply(input : Input) : Output = findRule(input) match { 
-    										case Some(ruleCase) => {
-    										  if (logger.isDebugEnabled()) {
-    										    logger.debug(s"rule [${ruleCase.label}] fired on input [${input}]")
-    										  }
-    										  ruleCase.returnValue
-    										}
-    										case None => defaultResult
-    									}
-  
-  def merge(other : RuleSet[Input, Output]) : RuleSet[Input, Output] =
-		  new RuleSet[Input, Output](this.rules ++ other.rules, this.defaultResult)
-		  
-  def modifySalience(modifySalience : Int => Int) : RuleSet[Input, Output] = 
-    	new RuleSet[Input, Output](this.rules map (x => (x._1, x._2.copy(salience = modifySalience(x._2.salience)))), 
-    								defaultResult)
-
-  override def andThen[OtherOutput](other : Output => OtherOutput) : RuleSet[Input, OtherOutput] = {
-    new RuleSet[Input, OtherOutput](
-        this.rules map (x => (x._1, x._2.andThen(other))),
-        other.apply(this.defaultResult))
-  }
-  
-  override def compose[PreInput](g : PreInput => Input) : RuleSet[PreInput, Output] =
-    new RuleSet[PreInput, Output](
-        this.rules map (x => (x._1, x._2.compose(g))),
-        this.defaultResult
-    )
-
-  def crossProduct[SecondInput, SecondOutput]( second : RuleSet[SecondInput, SecondOutput]) :
-		  														  RuleSet[(Input, SecondInput), (Output, SecondOutput)] = 
-		  														    new RuleSet[(Input, SecondInput), (Output, SecondOutput)](
-		  														    			for { firstRuleCase <- this.rules; secondRuleCase <- second.rules } yield ("<"+firstRuleCase._2.label + ":" + secondRuleCase._2.label +">",
-		  														    			    new RuleCase[(Input, SecondInput), (Output, SecondOutput)](
-		  														    			    label = "<"+firstRuleCase._2.label + ":" + secondRuleCase._2.label +">",
-		  														    			    matchExpr = new AndExpr[(Input, SecondInput)](List(firstRuleCase._2.matchExpr.compose(_._1), secondRuleCase._2.matchExpr.compose(_._2))),
-		  														    			    returnValue = (firstRuleCase._2.returnValue, secondRuleCase._2.returnValue),
-		  														    			    salience = firstRuleCase._2.salience * secondRuleCase._2.salience /* TODO is it a good idea to multiply */)),
-
-		  														    			(this.defaultResult, second.defaultResult))
 }
 
 
